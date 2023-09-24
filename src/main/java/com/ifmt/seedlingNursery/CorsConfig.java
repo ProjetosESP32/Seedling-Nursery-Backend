9a9bd6f168a7f9ebc68fe42ba67e63a6ee80cb16
Sadlark1993 <claudio.zi.junior@gmail.com>
2023-09-24 15:29:27 -0400

    configured CORS to allow request from everywere

---
+++ src/main/java/com/ifmt/seedlingNursery/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.ifmt.seedlingNursery;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+      }
+    };
+  }
+
+}
