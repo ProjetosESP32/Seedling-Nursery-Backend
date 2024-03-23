42f5839640cc2852c1e8fa3e5eb753609411b2ab
Sadlark1993 <claudio.zi.junior@gmail.com>
2024-03-22 23:29:43 -0400

    droping unecessary code

--- src/main/java/com/ifmt/seedlingNursery/security/SecurityConfig.java
+++ src/main/java/com/ifmt/seedlingNursery/security/SecurityConfig.java
@@ -5,16 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import com.ifmt.seedlingNursery.security.filter.AuthenticationFilter;
 
-import static org.springframework.security.config.Customizer.withDefaults;
 import com.ifmt.seedlingNursery.security.filter.ExceptionHandlerFilter;
 import com.ifmt.seedlingNursery.security.filter.JWTAuthorizationFilter;
 import com.ifmt.seedlingNursery.security.manager.CustomAuthenticationManager;
